bba010d0e22af34642f3706602e194c44efe23d2
DCx91 <186149186+DCx91@users.noreply.github.com>
2026-06-12 09:59:56 +0100

    Update unbound-healthcheck.sh

--- unbound/rootfs/usr/local/bin/unbound-healthcheck.sh
+++ unbound/rootfs/usr/local/bin/unbound-healthcheck.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/with-contenv bashio
-
-LISTEN_PORT=$(bashio::config 'listen_port')
+LISTEN_PORT=$(bashio::app.port '5335/udp')
 
 # Check if Unbound is listening on the configured port
-if ! drill -p "${LISTEN_PORT}" unbound.net @127.0.0.1 > /dev/null 2>&1; then
+if ! dig -p "${LISTEN_PORT}" . NS @127.0.0.1 > /dev/null 2>&1; then
     bashio::log.warning "Healthcheck: DNS query to Unbound on port ${LISTEN_PORT} failed."
     exit 1
 fi
